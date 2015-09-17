79f120a1d86a7cd3fb0ff5f44a79dc63b9a5cacd
Phillip Kriegel <philkrie@gmail.com>
2015-09-16 23:40:22 -0700

    Added hover jqeuery plugin to place overlay over image

---
+++ phil_subtheme/js/overlay.js
@@ -0,0 +1,10 @@
+(function($) {
+$(document).ready(function(){
+            $('#education').contenthover({
+            effect:'slide',
+            slide_speed:300,
+            overlay_background: '#000',
+            overlay_opacity: 0.8
+            });
+        });
+})(jQuery);
